2afc6638be97f7885b85306210b033c1bf2d3757
desarrollo <desarrollo@grupoelemental.com.co>
2019-12-05 16:28:35 -0500

    0.1

---
+++ src/Aplinsa.php
@@ -0,0 +1,26 @@
+<?php 
+
+	// Directorio: Proyecto/Prueba/MiClase.php
+	//namespace Proyecto\Prueba;
+
+	//directorio avalohack/integracionAplinsa
+	namespace avalohack\integracionAplinsa;
+
+	//si se requiere otra libreria use nombre libreria
+	/**
+	 * 
+	 */
+	class Aplinsa  
+	{		
+		function __construct(argument)
+		{
+			# code...
+		}
+
+		private function prueba(){
+
+			return "prueba text";
+		}
+	}
+
+	//https://diego.com.es/namespaces-en-php
